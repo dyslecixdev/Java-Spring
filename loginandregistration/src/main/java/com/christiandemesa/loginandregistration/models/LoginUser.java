54e2a13f8da3aa5693fda56a809937c0b18326bb
Christian Dean Demesa <christiandeandemesa@gmail.com>
2021-12-10 10:37:38 -0800

    Added comments

--- loginandregistration/src/main/java/com/christiandemesa/loginandregistration/models/LoginUser.java
+++ loginandregistration/src/main/java/com/christiandemesa/loginandregistration/models/LoginUser.java
@@ -4,20 +4,23 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+// This model will be used to login a user.
 // Notice there is no @Entity.
 public class LoginUser {
     
+	// Member variables with validations and error messages.
+	// Notice how the only member variables are those used to login.
     @NotEmpty(message="Email is required!")
     @Email(message="Please enter a valid email!")
     private String email;
-    
     @NotEmpty(message="Password is required!")
-    @Size(min=8, message="Password must be at least 8 characters")
+    @Size(min=8, message="Password must be at least 8 characters!")
     private String password;
     
     // Constructor
     public LoginUser() {
     }
+    // There is no overloaded constructor because this information is not saved to the database.
     
     // Getters and setters
 	public String getEmail() {
